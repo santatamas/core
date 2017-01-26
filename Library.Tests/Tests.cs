b8d9d897b7c83245cdf595338df76dd7d44dccd5
Tamas Santa <tamas.santa@infinityworks.com>
2017-01-26 15:03:12 +0000

    added test project and sample unit test

---
+++ Library.Tests/Tests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xunit;
+using Library;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1()
+        {
+            var result = new LibraryClass().Test(4);
+            Assert.Equal(8, result);
+        }
+    }
+}
--- Library/Library.cs
+++ Library/Library.cs
@@ -5,8 +5,13 @@ namespace Library
     public class LibraryClass
     {
         public void LibraryMethod()
-        {    
+        {
             Console.WriteLine("Library is working.");
+        }
+
+        public int Test(int input)
+        {
+            return input + input;
         }
     }
 }
